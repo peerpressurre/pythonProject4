42ae852479fd7bb99f5e264f02adfd56b3aa9d2b
peerpressurre <gsophiay@gmail.com>
2022-09-30 15:41:07 +0300

    Update: Feature/hm_ex_4

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-num = int(input('enter number->'))
+num1 = int(input('enter first number->'))
+num2 = int(input('enter second number->'))
 res = ''
 
-if num > 0:
-    res = 'Number is positive'
-elif num < 0:
-    res = 'Number is negative'
-elif num == 0:
-    res = 'Number is equal to zero'
+if num1 == num2:
+    res = 'Numbers are equal'
+elif num1 < num2:
+    res = num1, num2
+elif num1 > num2:
+    res = num2, num1
 
-print(f'{res}'
+print(f'{res}')
