326df28218f733de902fb3a09a36ab0d97ef983c
peerpressurre <gsophiay@gmail.com>
2022-09-30 15:19:55 +0300

    Update: Feature/hm_ex_3

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-num = int(input('enter a month number->'))
-month = ''
+num = int(input('enter number->'))
+res = ''
 
-if num == 1:
-    month = 'January'
-elif num == 2:
-    month= 'February'
-elif num == 3:
-    month = 'March'
-elif num == 4:
-    month = 'April'
-elif num == 5:
-    month = 'May'
-elif num == 6:
-    month = 'June'
-elif num == 7:
-    month = 'July'
-elif num == 8:
-    month = 'August'
-elif num == 9:
-    month = 'September'
-elif num == 10:
-    month = 'October'
-elif num == 11:
-    month = 'November'
-elif num == 12:
-    month = 'December'
+if num > 0:
+    res = 'Number is positive'
+elif num < 0:
+    res = 'Number is negative'
+elif num == 0:
+    res = 'Number is equal to zero'
 
-print(f'{month}'
-
-
+print(f'{res}')
